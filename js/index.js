9be060ddbdcaa8a2f955a1f259fd21ebe70f8696
Jie Li <jie.li@ptmind.com>
2017-08-30 13:48:04 +0800

    push

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
+
+  $(window).scroll(function() {
+        var t = $(this).scrollTop();
+        if (t > 300) {
+            $(".top").stop().fadeIn(300);
+        } else {
+            $(".top").stop().fadeOut(300);
+        }
+    });
+    $(".top").click(function() {
+        $("body,html").stop().animate({
+            scrollTop: 0
+        }, 300); //html是为了兼容火狐和IE
+    });
+    
 });
